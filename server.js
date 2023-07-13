3feef625c24a2deb800e62cc90befc4ca15c2a7a
Hackerdude374 <robertle982@gmail.com>
2023-07-13 15:13:25 -0400

    some more server.js changes

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ const {RecipeApplication} = require('./models');
 
 //create new recipe
 
-application.post("/recipes", async(req,res)=>{
+app.post("/recipes", async(req,res)=>{
 try{
     const newRecipe = await RecipeApplication.create(req.body);
-    
+    res.status(201).json(newRecipe);
+}catch(err){
+    console.error(err);
+    res.status(500).send({message: err.message});
 }
+});
+
+//get all recipes 
+app.get("/recipes", async (req,res) =>{
+    try{
+const allRecipes = await RecipeApplication.findAll();
+res.status(200).json(allRecipes);
+    }catch(err){
+        console.error(err);
+        res.status(500).send({message: err.message});
+    }
 });
