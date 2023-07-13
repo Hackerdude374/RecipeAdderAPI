98dea57252a7fa02c607c77dd2727fa077989da7
Hackerdude374 <robertle982@gmail.com>
2023-07-13 15:33:30 -0400

    changes to models/recipeapplication.js

--- models/recipeapplication.js
+++ models/recipeapplication.js
@@ -3,23 +3,30 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class RecipeApplication extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
+  class RecipeApplication extends Model {}
+
   RecipeApplication.init({
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    ingredients: DataTypes.STRING,
-    instructions: DataTypes.STRING,
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE
+    title: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull :false,
+    },
+    ingredients: {type: DataTypes.STRING,
+      allowNull: false,
+    },
+    instructions:{ type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull : false,
+    },
+    updatedAt:{ type: DataTypes.DATE,
+      allowNull: false,
+    },
   }, {
     sequelize,
     modelName: 'RecipeApplication',
--- server.js
+++ server.js
@@ -22,3 +22,60 @@ res.status(200).json(allRecipes);
         res.status(500).send({message: err.message});
     }
 });
+
+//get specigic recipe
+
+app.get("/recipes/:id", async (req,res)=>{
+const recipeId = parseInt(req.params.id, 10);
+try{
+    const recipe = await RecipeApplication.findOne({where: {id: recipeId}});
+
+if(recipe){
+    res.status(200).json(recipe);
+}else{
+    res.status(404).send({message: "recipe not found"});
+}
+}catch(err){
+    console.error(err);
+    res.status(500).send({message: err.message});
+}
+
+});
+
+//update a specific recipe
+app.patch("/recipes/:id", async (req, res) => {
+    const recipeId = parseInt(req.params.id, 10);
+  
+    try {
+      const [numberOfAffectedRows, affectedRows] = await RecipeApplication.update(req.body, { where: { id:
+         recipeId }, returning: true });
+  
+      if (numberOfAffectedRows > 0) {
+        res.status(200).json(affectedRows[0]);
+      } else {
+        res.status(404).send({ message: "recipe not found" });
+      }
+    } catch (err) {
+      res.status(500).send({ message: err.message });
+      console.error(err);
+    }
+  });
+
+
+  //delete a specific recipe\
+  app.delete("/recipes/:id", async (req, res) => {
+    const recipeId = parseInt(req.params.id, 10);
+    try {
+      const deleteOp = await RecipeApplication.destroy({ where: { id: recipeId } });
+      if (deleteOp > 0) {
+        res.status(200).send({ message: "recipe deleted successfully" });
+      } else {
+        res.status(404).send({ message: "recipe not found" });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ message: err.message });
+    }
+  });
+
+  
